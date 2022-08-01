0c898138c20090af1590249593f501364807a9c8
Rhenn Handel <rwhandel@gmail.com>
2022-08-01 19:19:28 -0400

    Hit det. working
    Getting a little frustrated

--- spells/SpellEffect.gd
+++ spells/SpellEffect.gd
@@ -2,19 +2,23 @@ extends Node2D
 
 export var accel := 400.0
 
-var velocity := 100.0
+var velocity := 0
 var direction := Vector2(1.0,-1.0)
 
 onready var _animation := $AnimationPlayer
 onready var _collision := $Area2D
+onready var _particles := $Particles2D
 
-func ready() -> void:
+func _ready() -> void:
 	_collision.connect("body_entered", self, "_on_Area_body_entered")
+	_particles.emitting = false
+	_particles.one_shot = true
+	pass
 	
 func _physics_process(delta: float) -> void:
-	position += direction * ((velocity * delta) + (0.5 * accel * pow(delta, 2)))
+	velocity += accel * delta
+	position += direction * velocity * delta
 
 
-func _on_Area2D_body_entered(body) -> void:
-	print("wtf")
+func _on_Area_body_entered(body) -> void:
 	_animation.play("Collision")
