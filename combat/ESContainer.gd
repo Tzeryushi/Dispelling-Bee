3b5c5128de9b3d9061749d3544f6a9c77633530d
Rhenn Handel <rwhandel@gmail.com>
2022-08-11 20:50:03 -0400

    bubbles and text centering
    Godot requires the user waits until an idle frame in order to set a pos in a container whose position is implicitly managed by another position. I think I'll just have to remove the panel container and babysit the whole thing with scripts.

--- combat/ESContainer.gd
+++ combat/ESContainer.gd
@@ -1,15 +1,25 @@
 extends PanelContainer
 
-onready var text_box = $ESpellText 
+onready var text_box = $ESpellText
+onready var bubble = $Bubble
+
+export var bg_color : Color = Color (0,0,1,1)
 
 var cached_text : String
+
+func _ready() -> void:
+	bubble.modulate = bg_color
 
 func set_text(text:String) -> void:
 	text_box.bbcode_text = text
 	if not get_tree() == null:
 		yield(get_tree(), "idle_frame")
 	var height = text_box.get_content_height()
-	rect_size.y = height
+	rect_size.y = height + 40.0
+	text_box.margin_top = (rect_size.y/2) - (height/2)
+	if not get_tree() == null:
+		yield(get_tree(), "idle_frame")
+	text_box.margin_top = (rect_size.y/2) - (height/2)
 	
 #	var clean_text = text_box.text
 #	var sanitized = ""
--- combat/PSContainer.gd
+++ combat/PSContainer.gd
@@ -1,15 +1,25 @@
 extends PanelContainer
 
 onready var text_box = $PSpellText 
+onready var bubble = $Bubble
+
+export var bg_color : Color = Color (0,0,1,1)
 
 var cached_text : String
+
+func _ready() -> void:
+	bubble.modulate = bg_color
 
 func set_text(text:String) -> void:
 	text_box.bbcode_text = text
 	if not get_tree() == null:
 		yield(get_tree(), "idle_frame")
 	var height = text_box.get_content_height()
-	rect_size.y = height + 8
+	rect_size.y = height + 80.0
+	text_box.margin_top = (rect_size.y/2) - (height/2)
+	if not get_tree() == null:
+		yield(get_tree(), "idle_frame")
+	text_box.margin_top = (rect_size.y/2) - (height/2)
 
 func change_text_color(color:Color) -> void:
 	text_box.add_color_override("default_color", color)
