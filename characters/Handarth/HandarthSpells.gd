b8c162bf5b4cb2f3d3c6f33ab128053fc30abd12
Rhenn Handel <rwhandel@gmail.com>
2022-08-27 19:00:18 -0400

    Handarth assets and scene

---
+++ characters/Handarth/HandarthSpells.gd
@@ -0,0 +1,9 @@
+extends EnemySpells
+
+func _ready() -> void:
+	list.append({"Text":"PooPoo!", "Tags":"[shake]", "Solve":"oopoop!", "Damage":1, "Drain":1, "Speed":15, "Animation":0})
+	list.append({"Text":"Stinky!", "Tags":"[shake][wave]", "Solve":"yknits!", "Damage":1, "Drain":1, "Speed":15, "Animation":0})
+	list.append({"Text":"Abluhluhb", "Tags":"[shake][wave]", "Solve":"bhulhulba", "Damage":1, "Drain":1, "Speed":15, "Animation":0})
+	list.append({"Text":"Crabby cake bake", "Tags":"[shake][wave]", "Solve":"ekab ekac ybbarc", "Damage":2, "Drain":3, "Speed":20, "Animation":0})
+	list.append({"Text":"Wacka Wacko Wingwam!", "Tags":"[rainbow]", "Solve":"mawgniw okcaw akcaw!", "Damage":2, "Drain":2, "Speed":20, "Animation":0})
+	list.append({"Text":"Shake it up!", "Tags":"[shake][wave]", "Solve":"pu ti ekahs!", "Damage":1, "Drain":2, "Speed":20, "Animation":0})
