e2cff722e39182a928c463d1c01d3bdcf395b313
Rhenn Handel <rwhandel@gmail.com>
2022-08-31 20:53:29 -0400

    Resource for enemy spells

--- data/EnemySpellBody.gd
+++ data/EnemySpellBody.gd
@@ -3,8 +3,11 @@ extends Resource
 class_name EnemySpell
 
 export var name : String
+export var solve : String
 export var shake_tag : bool = false
 export var wave_tag : bool = false
+export var tornado_tag : bool = false
+export var fade_tag : bool = false
 export var rainbow_tag : bool = false
 export var custom_tag : String = ""
 export var damage : int = 1
