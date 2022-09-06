aa513bc65afaea0b4e3ddaaf3626113280d5b110
Rhenn Handel <rwhandel@gmail.com>
2022-09-05 20:07:47 -0400

    quick color override

--- combat/CombatBackground.gd
+++ combat/CombatBackground.gd
@@ -4,7 +4,12 @@ export var speed : float = 50.0
 export var rot_speed : float = 0.1
 export var noise : float = 0.083
 
+export var color_override : Color = Color(1,1,1,1)
+
 var direction = Vector2(0,1)
+
+func _ready() -> void:
+	$ParallaxLayer/Sprite.modulate = color_override
 
 func _process(delta) -> void:
 	scroll_offset += direction * speed * delta
