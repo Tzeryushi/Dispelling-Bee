8c2bd0a27892579ec57a835770fcbe04d9cb4b6e
Rhenn Handel <rwhandel@gmail.com>
2022-09-26 20:17:57 -0400

    Setting up for animations

--- combat/Player.gd
+++ combat/Player.gd
@@ -7,3 +7,9 @@ func flash_color(color:Color, flash_time = 0.05, flashes = 2) -> void:
 	for _i in range(0, flashes):
 		tween.tween_property(image, "modulate", color, flash_time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 		tween.tween_property(image, "modulate", Color(1,1,1,1), flash_time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+
+func attack() -> void:
+	pass
+	
+func idle_loop() -> void:
+	pass
