cbdefa280e6c4013012c8bb6b8e271174b8725e9
Rhenn Handel <rwhandel@gmail.com>
2022-09-28 19:21:18 -0400

    fixed animation issues
    used a workaround, the animation tree is a mess.

--- combat/Player.gd
+++ combat/Player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var image := $Pos/PlayerImage
+onready var api_animation = $Pos/PlayerImage/ApiAnimations
 onready var api_animation_state = $Pos/PlayerImage/ApiAnimationTree.get("parameters/playback")
 
 func flash_color(color:Color, flash_time = 0.05, flashes = 2) -> void:
@@ -10,10 +11,14 @@ func flash_color(color:Color, flash_time = 0.05, flashes = 2) -> void:
 		tween.tween_property(image, "modulate", Color(1,1,1,1), flash_time).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 
 func attack() -> void:
-	api_animation_state.travel("casting")
+	#api_animation_state.travel("casting")
+	api_animation.play("casting")
+	api_animation.queue("idle")
 	
 func idle_loop() -> void:
-	api_animation_state.travel("idle")
+	#api_animation_state.travel("idle")
+	api_animation.play("idle")
 
 func pause() -> void:
-	api_animation_state.travel("RESET")
+	api_animation.stop(true)
+	#api_animation_state.travel("RESET")
