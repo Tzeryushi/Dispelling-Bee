1f04d409cee6b6109037f8fcae9e1641e9c861ae
Rhenn Handel <rwhandel@gmail.com>
2022-09-30 15:54:26 -0400

    Added soundtrack

--- combat/CombatAudio.gd
+++ combat/CombatAudio.gd
@@ -2,6 +2,9 @@ extends Node
 
 onready var speech_player = $SpeechPlayer
 
+func _ready():
+	$Groovin.play()
+
 func play_speech(key:String) -> bool:
 	if speech_player.lookup_and_queue(key):
 		return true
