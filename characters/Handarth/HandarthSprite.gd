7da917de01d9e8d18716a78e021228a2796f5ba4
Rhenn Handel <rwhandel@gmail.com>
2022-10-23 03:19:00 -0400

    Dispel and Hurt anims for Handarth

--- characters/Handarth/HandarthSprite.gd
+++ characters/Handarth/HandarthSprite.gd
@@ -3,7 +3,18 @@ extends Node2D
 onready var body = $Pos/Image
 onready var hand_1 = $Pos/Image/HandPivot1
 onready var hand_2 = $Pos/Image/HandPivot2
+onready var player := $Pos/AnimationPlayer
 
 func attack() -> void:
 	hand_1.attack()
 	hand_2.attack()
+
+func hurt() -> void:
+	player.play("Hurt")
+	yield(player, "animation_finished")
+	player.play("Idle")
+
+func dispelled() -> void:
+	player.play("Dispelled")
+	yield(player, "animation_finished")
+	player.play("Idle")
