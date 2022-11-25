552a5f6a13e860071936e6f317b079578be50eec
Rhenn Handel <rwhandel@gmail.com>
2022-11-24 21:14:14 -0500

    symmantis loaded assets and scene

---
+++ SymmantisSprite.gd
@@ -0,0 +1,24 @@
+extends DefaultWizardSprite
+
+onready var player = $Pos/AnimationPlayer
+
+func attack() -> void:
+	pass
+
+func attack2() -> void:
+	pass
+
+func hurt() -> void:
+	pass
+
+func dispelled() -> void:
+	pass
+
+func channel() -> void:
+	pass
+
+func lose() -> void:
+	pass
+	
+func win() -> void:
+	pass
