dc822177b04b86ef28f4affbc3b8b9b5dc7b5d15
Rhenn Handel <rwhandel@gmail.com>
2022-12-09 18:26:31 -0500

    library fix and new attack animation

--- characters/Symmantis/Symmantis.gd
+++ characters/Symmantis/Symmantis.gd
@@ -2,3 +2,7 @@ extends "res://combat/DefaultWizard.gd"
 
 var symmetric := true
 
+#TODO:
+#Implement next_spell() - this should also include switch counter
+	#should channel when the switch is made
+#custom weighted index spell (pass in spell list as parameter)
