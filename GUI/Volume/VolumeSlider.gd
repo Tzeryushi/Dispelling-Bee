39ef22662d9bf52ac9796501f153ba05ecf464bc
Rhenn Handel <rwhandel@gmail.com>
2023-01-15 18:54:49 -0500

    Volume settings, display changes

--- GUI/Volume/VolumeSlider.gd
+++ GUI/Volume/VolumeSlider.gd
@@ -1,2 +1,11 @@
-extends HSlider
+extends Node
 
+onready var slider = $Control/VBoxContainer/HBoxContainer/VBoxContainer/VolumeSlider
+
+func _ready():
+	print("ready is called")
+	var vol = AudioServer.get_bus_volume_db(0)
+	slider.value = vol
+
+func _on_VolumeSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(0,value)
