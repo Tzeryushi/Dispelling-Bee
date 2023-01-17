ed06690e939f3f1d4f52d139d63e3c4bcbc4ac10
Rhenn Handel <rwhandel@gmail.com>
2023-01-17 01:13:50 -0500

    more tutorial work

--- GUI/TutorialGUI/Tutorial.gd
+++ GUI/TutorialGUI/Tutorial.gd
@@ -2,6 +2,14 @@ extends Node2D
 
 export(Array, PackedScene) var panels
 
+onready var right_button := $GUI/RightButton
+onready var left_button := $GUI/LeftButton
+onready var right_arrow := $GUI/RightButton/Arrow
+onready var left_arrow := $GUI/LeftButton/Arrow
+onready var gui := $GUI
+
+var current_panel : TutPanel
+
 #animate opening
 #contain all separate tutpanel packedscenes
 #grab focus upon opening
@@ -9,5 +17,56 @@ export(Array, PackedScene) var panels
 #animate closing
 #send signal back to menu scene to regrab focus
 
+#should I have put the buttons in the base tutorial scene? absolutely!
+#do I want to go back and change it? absolutely not!
+
 func _ready() -> void:
+	if !panels.empty():
+		current_panel = panels[0].instance()
+		gui.add_child(current_panel)
+	#get_focus()
+
+func is_focused() -> bool:
+	if gui.get_focus_owner() == right_button:
+		return true
+	elif gui.get_focus_owner() == left_button:
+		return true
+	return false
+
+func get_focus() -> void:
+	right_button.grab_focus()
+
+func next_scene(forward:bool) -> void:
+	#add scene, forwards or back in array
+	#connect new scene's signals
 	pass
+
+#Right Button
+func _on_RightButton_pressed():
+	emit_signal("right_pressed")
+
+func _on_RightButton_mouse_entered():
+	_light_right(Color(2.0, 2.0, 2.0))
+func _on_RightButton_mouse_exited():
+	_light_right(Color(1.0, 1.0, 1.0))
+func _on_RightButton_focus_entered():
+	_light_right(Color(2.0, 2.0, 2.0))
+func _on_RightButton_focus_exited():
+	_light_right(Color(1.0, 1.0, 1.0))
+func _light_right(value:Color) -> void:
+	right_arrow.modulate = value
+
+#Left Button
+func _on_LeftButton_pressed():
+	emit_signal("left_pressed")
+
+func _on_LeftButton_mouse_entered():
+	_light_left(Color(2.0, 2.0, 2.0))
+func _on_LeftButton_mouse_exited():
+	_light_left(Color(1.0, 1.0, 1.0))
+func _on_LeftButton_focus_entered():
+	_light_left(Color(2.0, 2.0, 2.0))
+func _on_LeftButton_focus_exited():
+	_light_left(Color(1.0, 1.0, 1.0))
+func _light_left(value:Color) -> void:
+	left_arrow.modulate = value
--- TempMenu.gd
+++ TempMenu.gd
@@ -6,17 +6,23 @@ onready var container = $Control/VBoxContainer
 onready var pause = $CanvasLayer/Pause
 
 export var flags : Resource
+export var tutorial_scene : PackedScene
 var theme = SoundtrackManager.THEME.MENU
+var tutorial_window
+var tut_focus : bool = false
 
 signal request_quit
 
 func _ready() -> void:
 	for button in pause.container.get_children():
 		button.connect("select", self, "_p_selected")
+	open_tutorial()
 	
 func _input(event) -> void:
-	if !is_focused():
+	if !is_focused() and !tut_focus:
 		$Control/VBoxContainer/Button.grab_focus()
+	elif !tutorial_window.is_focused() and tut_focus:
+		tutorial_window.get_focus()
 	
 func is_focused() -> bool:
 	for button in container.get_children():
@@ -32,6 +38,11 @@ func focus_update() -> void:
 			button.check.visible = true
 		else:
 			button.check.visible = false
+
+func open_tutorial() -> void:
+	tutorial_window = tutorial_scene.instance()
+	add_child(tutorial_window)
+	tut_focus = true
 
 func _p_selected(id) -> void:
 	match id:
