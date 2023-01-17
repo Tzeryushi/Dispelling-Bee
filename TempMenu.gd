6d6212e7ad8ae736a1605775aabdc564bf85f078
Rhenn Handel <rwhandel@gmail.com>
2023-01-17 14:39:17 -0500

    tutorial button and linking

--- TempMenu.gd
+++ TempMenu.gd
@@ -4,6 +4,7 @@ class_name TempMenu
 
 onready var container = $Control/VBoxContainer
 onready var pause = $CanvasLayer/Pause
+onready var tut_button = $Control/TutorialButton
 
 export var flags : Resource
 export var tutorial_scene : PackedScene
@@ -11,23 +12,26 @@ var theme = SoundtrackManager.THEME.MENU
 var tutorial_window
 var tut_focus : bool = false
 
+
 signal request_quit
 
 func _ready() -> void:
 	for button in pause.container.get_children():
 		button.connect("select", self, "_p_selected")
-	open_tutorial()
 	
 func _input(event) -> void:
 	if !is_focused() and !tut_focus:
 		$Control/VBoxContainer/Button.grab_focus()
-	elif !tutorial_window.is_focused() and tut_focus:
-		tutorial_window.get_focus()
+	elif tut_focus:
+		if !tutorial_window.is_focused():
+			tutorial_window.get_focus()
 	
 func is_focused() -> bool:
 	for button in container.get_children():
 		if container.get_focus_owner() == button:
 			return true
+	if container.get_focus_owner() == tut_button:
+		return true
 	return false
 
 func focus_update() -> void:
@@ -39,14 +43,20 @@ func focus_update() -> void:
 		else:
 			button.check.visible = false
 
-func open_tutorial() -> void:
-	tutorial_window = tutorial_scene.instance()
-	add_child(tutorial_window)
-	tut_focus = true
-
 func _p_selected(id) -> void:
 	match id:
 		"resume":
 			return
 		"exit":
 			emit_signal("request_quit")
+
+func _on_TutorialButton_pressed():
+	open_tutorial()
+
+func open_tutorial() -> void:
+	tutorial_window = tutorial_scene.instance()
+	add_child(tutorial_window)
+	tut_focus = true
+	
+func close_tutorial() -> void:
+	tut_focus = false
